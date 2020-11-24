53a391a1ae1ac67d81f4477f2699d3eb45e10100
Nihal Bhovare <nihalblog25@gmail.com>
2020-11-24 20:46:44 +0530

    Added a method that returns how many times Add() was invoked

--- src/stringCalc/StringCalculator.java
+++ src/stringCalc/StringCalculator.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 
 public class StringCalculator {
 	
+	private int count=0;	
+	public int GetCalledCount()
+	{		
+		return count;
+	}
+	
 	public int Add(String str)
 	{
+		++count;
 		int sum=0;	
 		if(str.length()<1)
 			return 0;
-		
+							
 			for(int l=0;l<str.length();l++)
 			{
 				if(Character.isDigit(str.charAt(l)))
+				{										
 					sum+=Integer.valueOf(str.charAt(l))-48;
+				}					
 			}				
 		return sum;
 	}
@@ -25,6 +34,7 @@ public class StringCalculator {
 		
 		StringCalculator sc=new StringCalculator ();		
 		System.out.println("Sum = "+sc.Add(str));
+		System.out.println("Numbers of times Add method was called = "+sc.GetCalledCount());
 		s.close();
 		
 	}
