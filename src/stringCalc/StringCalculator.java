399131bc0255e72f45823358e0c7887c513dc387
Nihal Bhovare <nihalblog25@gmail.com>
2020-11-24 22:46:28 +0530

    Numbers bigger than 1000 should be ignored done

--- src/stringCalc/StringCalculator.java
+++ src/stringCalc/StringCalculator.java
@@ -12,20 +12,40 @@ public class StringCalculator {
 	public int Add(String str)
 	{
 		++count;
-		int sum=0;	
+		int sum=0,temp;;	
 		if(str.length()<1)
 			return 0;
-							
-			for(int l=0;l<str.length();l++)
+		
+		for(int l=0;l<str.length();l++)
+		{
+			if(Character.isDigit(str.charAt(l)))
 			{
-				if(Character.isDigit(str.charAt(l)))
-				{										
-					sum+=Integer.valueOf(str.charAt(l))-48;
-				}					
-			}			
-			
+				if(l>=str.length())
+					break;
+				
+				if(Character.isDigit(str.charAt(l+1)))
+				{
+					temp=l;				
+					while(Character.isDigit(str.charAt(l)) && l<str.length())
+					{
+						l++;
+						
+						if(l>=str.length())
+							break;
+					}						
+					
+					if(!(Integer.valueOf(str.substring(temp, l))>1000))
+						sum+=Integer.valueOf(str.substring(temp, l));
+					
+				}
+				else				
+					sum+=Integer.valueOf(str.charAt(l))-48;				
+			}				
+		}
+		
 		return sum;
 	}
+	
 	
 	public static void main(String[] args) {
 		
